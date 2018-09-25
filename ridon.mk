efdc73b642d2f073311e10f923b62d41944d25ec
princeofgiri <ahmadharis1982@gmail.com>
2018-09-25 07:43:32 +0700

    port to ridon

--- ridon.mk
+++ ridon.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, device/huawei/angler/aosp_angler.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_angler
+PRODUCT_NAME := ridon_angler
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 6P
 
